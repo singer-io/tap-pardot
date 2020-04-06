4230d5b8985e4326daebb7e11c20f60194260024
Kyle Allan <kyle@stitchdata.com>
2020-04-06 13:53:06 +0000

    fix typo; bump to v0.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## 0.2.2
+  * Fix a naming error of Pardot5xxError
+
 ## 0.2.1
   * Retry's 5xx errors from Pardot [#5](https://github.com/singer-io/tap-pardot/pull/5)
--- tap_pardot/client.py
+++ tap_pardot/client.py
@@ -20,7 +20,7 @@ class PardotException(Exception):
 
 
 def is_not_retryable_pardot_exception(exc):
-    if isinstance(exc, Pardot5XXError):
+    if isinstance(exc, Pardot5xxError):
         return False
     if exc.code == 66:
         LOGGER.warn("Exceeded concurrent request limit, backing off exponentially.")
