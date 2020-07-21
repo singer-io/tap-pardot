a30eea147e6fe1abc5cfcee0a278ac7b3ac028de
Dan Mosora <dan@stitchdata.com>
2020-07-21 17:43:11 +0000

    Cleanup and add stop iteration scenario

--- tap_pardot/sync.py
+++ tap_pardot/sync.py
@@ -11,18 +11,18 @@ def translate_state(client, state, selected_streams):
     """
     selected_stream_names = [s.tap_stream_id for s in selected_streams]
     if "visitor_activities" in selected_stream_names:
-        bookmark_id = singer.bookmark.get_bookmark(state, "visitor_activities", "id")
+        bookmark_id = singer.bookmarks.get_bookmark(state, "visitor_activities", "id")
         if bookmark_id:
             bookmark_activity = client.get_specific("visitorActivity", bookmark_id)
             if bookmark_activity.get('visitor_activity', {}).get('updated_at'):
                 new_bookmark = utils.strftime(utils.strptime_to_utc(bookmark_activity['visitor_activity']['updated_at']))
-                singer.bookmark.clear_bookmark(state, "visitor_activities", "id")
-                singer.bookmark.write_bookmark(state, "visitor_activities", "window_start", bookmark_activity['updated_at'])
+                singer.bookmarks.clear_bookmark(state, "visitor_activities", "id")
+                singer.bookmarks.write_bookmark(state, "visitor_activities", "window_start", bookmark_activity['updated_at'])
             else:
                 raise Exception("Could not translate state for visitor_activites, bookmarked activity is missing `updated_at` value")
 
 def sync(client, config, state, catalog):
-    selected_streams = catalog.get_selected_streams(state)
+    selected_streams = list(catalog.get_selected_streams(state))
     translate_state(client, state, selected_streams)
 
     for stream in selected_streams:
